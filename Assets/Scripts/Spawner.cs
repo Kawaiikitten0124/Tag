f2afa79076e9026662fd9e62334a9bee5eb55445
Kawaiikitten0124 <apoultne@student.fitchburgstate.edu>
2024-10-03 12:02:29 -0400

    done game!

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 
@@ -10,35 +13,39 @@ public class Spawner : MonoBehaviour
     private float spawnRangeZ = 10;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
-    private float targetTime = 10.0f;
+    public Transform Spheretarget;
+    public float targetTime;
+    public Text TimerText;
     //private bool GameActive = true;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnWave", startDelay, spawnInterval); 
+        InvokeRepeating("SpawnWave", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-            if (Input.GetKeyUp(KeyCode.S))
-      {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
             SpawnWave();
 
-      }
-        targetTime = Time.deltaTime;
-        if (targetTime <=0.0f)
+        }
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f)
         {
             TimeEnded();
+            targetTime = 0.0f;
         }
-    } 
+        TimerText.text = ((int)targetTime).ToString();
+    }
     void SpawnRandomSphere()
     {
         int SphereIndex = Random.Range(0, SpherePrefab.Length);
         Vector3 SpawnRange = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, (Random.Range(-spawnRangeZ, spawnRangeZ)));
 
 
-        Instantiate(SpherePrefab[SphereIndex], SpawnRange, SpherePrefab[SphereIndex].transform.rotation); 
+        Instantiate(SpherePrefab[SphereIndex], SpawnRange, SpherePrefab[SphereIndex].transform.rotation, Spheretarget);
     }
     void SpawnWave()
     {
@@ -51,5 +58,7 @@ public class Spawner : MonoBehaviour
     void TimeEnded()
     {
         CancelInvoke();
+        Destroy(Spheretarget.gameObject);
     }
-}
+
+}  
